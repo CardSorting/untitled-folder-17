4260b26d342673790152629364868cf3d4c1b42e
PlayMoreTCG <playmoretcg@gmail.com>
2024-12-24 16:10:24 -0700

    yamlbroke

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,10 +1,14 @@
 import multiprocessing
+import os
 
 # Gunicorn config variables
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 2  # Reduced number of workers for free tier
 worker_class = "gevent"
-bind = "0.0.0.0:$PORT"
+bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
 timeout = 120
 keepalive = 5
 max_requests = 1000
 max_requests_jitter = 50
+
+# Prevent gevent monkey-patching warning
+preload_app = True
