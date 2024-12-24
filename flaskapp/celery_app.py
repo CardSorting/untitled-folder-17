536b8d885148f6a460ebe39703683ec657d509e3
PlayMoreTCG <playmoretcg@gmail.com>
2024-12-24 12:42:07 -0700

    tryingtofixpipeline

--- flaskapp/celery_app.py
+++ flaskapp/celery_app.py
@@ -1,7 +1,5 @@
-import os
-from flask import current_app
 from celery import Celery
-from flaskapp import create_app
+from . import create_app
 
 def make_celery(app):
     celery = Celery(
@@ -19,9 +17,6 @@ def make_celery(app):
         accept_content=['json'],
     )
     
-    # Update with any additional Flask config
-    celery.conf.update(app.config)
-
     class ContextTask(celery.Task):
         def __call__(self, *args, **kwargs):
             with app.app_context():
@@ -30,20 +25,9 @@ def make_celery(app):
     celery.Task = ContextTask
     return celery
 
-# Initialize Flask app and Celery
 flask_app = create_app()
 celery = make_celery(flask_app)
 
-# Register tasks from modules
-from .tasks import chat_tasks, companion_tasks
-
-# Register companion tasks
+# Import tasks to register them
+from .tasks import companion_tasks
 companion_task_registry = companion_tasks.register_tasks(celery)
-process_companion_chat = companion_task_registry['process_companion_chat']
-
-from .celery_app import celery
-
-__all__ = ['celery']
-
-if __name__ == '__main__':
-    celery.start()
---
+++ flaskapp/tasks/__init__.py
@@ -0,0 +1,4 @@
+"""Task package initialization."""
+from .companion_tasks import register_tasks as register_companion_tasks
+
+__all__ = [ 'register_companion_tasks']
